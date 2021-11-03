ab37b2108c9cc3f26838f35ee1f958327137a9bd
Malin Nilsson <malinnilsson@Malins-MacBook-Pro-2.local>
2021-11-03 15:54:27 +0100

    Restructured code, added function to add/delete li items

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 window.onload = function() {
-    addRemoveButton()
+    removeButton()
     deleteListItem()
-    lineThrough()
+
+    let addBtn = document.getElementById("add-btn");
+    addBtn.addEventListener("click", addListItem);
+
+    let list = document.querySelector("ul");
+    list.addEventListener("click", toggleFunction);
+}
+
+// Funktion som lägger/tar bort nya list-element
+function addListItem() {
+    let newToDo = document.createElement("li");
+    let input = document.getElementById("input").value;
+    newToDo.innerHTML = input;
+
+    if (input === "") {
+        alert("You forgot to write something!")
+    } else {
+        let span = document.createElement("span");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "remove";
+        span.appendChild(txt);
+        newToDo.appendChild(span);
+        list.appendChild(newToDo);
+    }
+    deleteListItem();
 }
 
 // Funktion som lägger till delete-knapp på varje listelement
-function addRemoveButton() {
+function removeButton() {
     let listItems = document.querySelectorAll("li");
     for (let i = 0; i < listItems.length; i++) {
         let span = document.createElement("span");
@@ -21,17 +45,12 @@ function deleteListItem() {
     let remove = document.getElementsByClassName("remove");
     for (let i = 0; i < remove.length; i++) {
         remove[i].addEventListener("click", function () {
-            let div = this.parentElement;
-            div.style.display = "none";
+            let parent = this.parentElement;
+            parent.style.display = "none";
         });
     }
 }
 
-// Funktion som stryker igenom text vid klick
-function lineThrough() {
-let list = document.querySelector("ul");
-list.addEventListener("click", toggleFunction)
-}
 
 // Funktion som växlar mellan vanlig text och genomstrukten text
 function toggleFunction(e) {
