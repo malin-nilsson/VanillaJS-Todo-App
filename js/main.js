997a8f93aae37ae276a7730fa2579b15db925159
”Malin <malinnilsson87@gmail.com>
2021-11-08 20:41:38 +0100

    Function for adding new todos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Create class for Todo objects
 class Todo {
-    constructor(input, status) {
-        this.input = input;
+    constructor(todo, status) {
+        this.todo = todo;
         this.isDone = status;
     }
 }
@@ -13,14 +13,14 @@ let myItem3 = new Todo("Meditate", false);
 let myItem4 = new Todo("Call Mom", false);
 let myItems = [myItem1, myItem2, myItem3, myItem4];
 let myItemsStatus = [myItem1.status, myItem2.status, myItem3.status, myItem4.status]
-let myItemsInput = [myItem1.input, myItem2.input, myItem3.input, myItem4.input]
+let myItemsInput = [myItem1.todo, myItem2.todo, myItem3.todo, myItem4.todo]
 
 let doneItems = []
 console.log("my items: " + myItemsInput)
 
 window.onload = function () {
 
-    // Create UL and append it to div container
+    // Create UL and append it to container div
     let ul = document.createElement("ul");
     ul.setAttribute("id", "list");
     let container = document.getElementById("list-container");
@@ -37,7 +37,7 @@ window.onload = function () {
         li.appendChild(checkbox);
     }
 
-    // Add event listeners to checkboxes to hide checkbox when it's clicked
+    // Add event listeners to checkboxes so they are hidden when they're clicked
     let check = document.getElementsByClassName("check");
     for (let i = 0; i < check.length; i++) {
         check[i].addEventListener("click", hideTodos)
@@ -65,7 +65,7 @@ window.onload = function () {
             checkBox[i].addEventListener("click", () => {
                 if (checkBox[i].checked) {
                     myItems[i].isDone = true; //set status to true if to-do is done
-                    let newtext = myItems[i].input; // grab input from to-do item
+                    let newtext = myItems[i].todo; // grab input from to-do item
 
                     // Create HTML for finished to-dos
                     let doneItem = document.createElement("li");
@@ -88,18 +88,34 @@ window.onload = function () {
     }
     checkOffFromList()
 
-
+    // Function to add todo back to list
     function addBackTodo() {
-      
-        }
 
     }
 
+    // Function to add new todo to list
+    let addbutton = document.getElementById("add-btn");
+    addbutton.addEventListener("click", addNewTodo);
 
+    function addNewTodo() {
+        let newTodoItem = document.createElement("li");
+        let input = document.getElementById("input").value;
+        newTodoItem.innerHTML = input;
+        ul.insertBefore(newTodoItem, ul.childNodes[0]);
 
+        let doneSpan = document.createElement("span");
+        doneSpan.classList.add("add-back");
+        newTodoItem.appendChild(doneSpan);
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox", "newcheckbox");
+        newTodoItem.appendChild(checkbox);
 
+        let newTodo = new Todo(newTodoItem.textContent, "false");
+        myItems.push(newTodo);
 
-
+    }
+}
 
 
 
