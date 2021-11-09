4fb0505319cd3772a16ad1dd2cd16257c06f7a4a
”Malin <malinnilsson87@gmail.com>
2021-11-09 20:19:19 +0100

    New functions for creating and checking off todos

--- js/main.js
+++ js/main.js
@@ -8,114 +8,89 @@ class Todo {
 
 // Create Todo objects
 let myItem1 = new Todo("Finish JS assignment", false);
-let myItem2 = new Todo("Go grocery shopping", false);
+let myItem2 = new Todo("Get groceries", false);
 let myItem3 = new Todo("Meditate", false);
 let myItem4 = new Todo("Call Mom", false);
 let myItems = [myItem1, myItem2, myItem3, myItem4];
-let myItemsStatus = [myItem1.status, myItem2.status, myItem3.status, myItem4.status]
-let myItemsInput = [myItem1.todo, myItem2.todo, myItem3.todo, myItem4.todo]
-
-let doneItems = []
-console.log("my items: " + myItemsInput)
 
 window.onload = function () {
 
-    // Create UL and append it to container div
+    createHTMLforTodo()
+
+    let addbutton = document.getElementById("add-btn"); // Grab the button 
+    addbutton.addEventListener("click", addNewTodo); // Add an event listener to the button
+
+}
+
+// Create HTML for todo
+function createHTMLforTodo() {
+    let container = document.getElementById("list-container");
+    container.innerHTML = "";
     let ul = document.createElement("ul");
     ul.setAttribute("id", "list");
-    let container = document.getElementById("list-container");
     container.appendChild(ul);
 
-    // Create HTML for Todo objects
-    for (i = 0; i < myItemsInput.length; i++) {
+    let finishedUL = document.createElement("ul");
+    finishedUL.classList.add("finished-ul")
+    container.appendChild(finishedUL);
+
+    let heading = document.createElement("h3");
+    heading.innerHTML = "Finished todos:"
+    ul.after(heading);
+    
+    for (let i = 0; i < myItems.length; i++) {
         let li = document.createElement("li");
-        li.textContent = myItemsInput[i]
-        ul.appendChild(li);
+        li.textContent = myItems[i].todo;
+
+        if (myItems[i].isDone) {
+            finishedUL.appendChild(li);
+            li.classList.add("finished");
+            
+        } else {
+            ul.appendChild(li);
+        }
+
         let checkbox = document.createElement("input")
         checkbox.type = "checkbox";
         checkbox.classList.add("check");
         li.appendChild(checkbox);
-    }
 
-    // Add event listeners to checkboxes so they are hidden when they're clicked
-    let check = document.getElementsByClassName("check");
-    for (let i = 0; i < check.length; i++) {
-        check[i].addEventListener("click", hideTodos)
-    }
-
-    // Function to hide to-dos
-    function hideTodos(e) {
-        let text = e.target.parentElement.textContent;
-        let parent = this.parentElement;
-        parent.style.display = "none";
-        doneItems.push(text); // push items to doneItems array
-        console.log("Done Items: " + doneItems);
-    }
-
-    // Create heading for finished todos
-    let heading = document.createElement("h3");
-    heading.classList.add("heading");
-    heading.innerHTML = "Finished items:";
-    ul.appendChild(heading);
-
-    // Function to check off todo and add to "Finished todos"
-    function checkOffFromList() {
-        let checkBox = document.getElementsByClassName("check");
-        for (let i = 0; i < checkBox.length; i++) {
-            checkBox[i].addEventListener("click", () => {
-                if (checkBox[i].checked) {
-                    myItems[i].isDone = true; //set status to true if to-do is done
-                    let newtext = myItems[i].todo; // grab input from to-do item
-
-                    // Create HTML for finished to-dos
-                    let doneItem = document.createElement("li");
-                    doneItem.textContent = newtext;
-                    doneItem.classList.add("finished")
-                    ul.appendChild(doneItem);
-                    let doneSpan = document.createElement("span");
-                    doneSpan.classList.add("add-back");
-                    doneItem.appendChild(doneSpan);
-                    let checkbox = document.createElement("input")
-                    checkbox.type = "checkbox";
-                    checkbox.classList.add("checkbox", "newcheckbox");
-                    checkbox.setAttribute("checked", "checked");
-                    doneItem.appendChild(checkbox);
-                }
-                console.log(myItems)
-                // remove(i);
-            });
-        }
-    }
-    checkOffFromList()
-
-    // Function to add todo back to list
-    function addBackTodo() {
-
-    }
-
-    // Function to add new todo to list
-    let addbutton = document.getElementById("add-btn");
-    addbutton.addEventListener("click", addNewTodo);
-
-    function addNewTodo() {
-        let newTodoItem = document.createElement("li");
-        let input = document.getElementById("input").value;
-        newTodoItem.innerHTML = input;
-        ul.insertBefore(newTodoItem, ul.childNodes[0]);
-
-        let doneSpan = document.createElement("span");
-        doneSpan.classList.add("add-back");
-        newTodoItem.appendChild(doneSpan);
-        let checkbox = document.createElement("input")
-        checkbox.type = "checkbox";
-        checkbox.classList.add("checkbox", "newcheckbox");
-        newTodoItem.appendChild(checkbox);
-
-        let newTodo = new Todo(newTodoItem.textContent, "false");
-        myItems.push(newTodo);
-
+        checkbox.addEventListener("change", () => {
+            markTodoAsDone(myItems[i])
+        });
     }
 }
+
+// Function to hide to-dos
+function markTodoAsDone(objectToMarkAsDone) {
+    objectToMarkAsDone.isDone = true;
+    createHTMLforTodo();
+}
+
+function addNewTodo() {
+    let newTodoItem = document.createElement("li");
+    let input = document.getElementById("input").value;
+    newTodoItem.innerHTML = input;
+
+    let container = document.getElementById("list-container");
+    let ul = document.createElement("ul");
+    ul.setAttribute("id", "list");
+    container.appendChild(ul);
+
+    ul.insertBefore(newTodoItem, ul.childNodes[0]);
+
+    let newCheckbox = document.createElement("input")
+    newCheckbox.type = "checkbox";
+    newCheckbox.classList.add("checkbox", "newcheckbox");
+    newTodoItem.appendChild(newCheckbox);
+
+    let newTodo = new Todo(newTodoItem.textContent, false);
+    myItems.push(newTodo);
+    console.log(myItems)
+
+}
+
+
 
 
 
