e5af93643d754b6fdfabe5207b1adffe481b1177
”Malin <malinnilsson87@gmail.com>
2021-11-10 22:27:54 +0100

    Add trash-can icons, prevent default on form

--- js/main.js
+++ js/main.js
@@ -17,23 +17,29 @@ window.onload = function () {
     createHTMLforTodo()
     let addbutton = document.getElementById("add-btn"); // Grab the button 
     addbutton.addEventListener("click", addNewTodo); // Add an event listener to listen for clicks
+    
 }
 
 // Create HTML for todo
 function createHTMLforTodo() {
     let container = document.getElementById("list-container"); // Grab container 
     container.innerHTML = ""; // Set it to empty
+    let headingContainer = document.createElement("div"); // Create container for subheading
+    headingContainer.classList.add("heading-container"); // Add class to subheading
+    let h2 = document.createElement("h2"); // Create an H2
+    h2.innerHTML = "Items";    // Set the HTML
+    container.appendChild(headingContainer);    // Append the container
+    headingContainer.appendChild(h2);  // Append the H2
     let ul = document.createElement("ul"); // Create a UL to hold the list items
     ul.setAttribute("id", "list"); // Add an ID to the UL
     container.appendChild(ul); // Append the UL to the container
-
     let finishedUL = document.createElement("ul"); // Create UL for finished todos
     finishedUL.classList.add("finished-list") // Add a class to the UL
     container.appendChild(finishedUL); // Append the UL to the container
+    let subheading = document.createElement("h3"); // Create an H3
+    subheading.innerHTML = "Completed:" // Add text to H3
+    ul.after(subheading); // Append it after the UL
 
-    let heading = document.createElement("h3"); // Create an H3
-    heading.innerHTML = "Finished to-dos:" // Add text to H3
-    ul.after(heading); // Append it after the UL
 
     // Loop through myTodos list and create LI:s for todos
     for (let i = 0; i < myTodos.length; i++) {
@@ -43,7 +49,7 @@ function createHTMLforTodo() {
         let checkbox = document.createElement("input") // Create an input element
         checkbox.type = "checkbox"; // Make it into a checkbox
         checkbox.classList.add("check"); // Add class to the checkbox
-        checkbox.setAttribute("title", "Check off the list"); // Add title attribute to checkboxes that shows a text on hover
+        checkbox.setAttribute("title", "Mark as done"); // Add title attribute to checkboxes that shows a text on hover
         li.appendChild(checkbox); // And append it to the LI
 
         // Add event listeners to checkboxes
@@ -55,11 +61,15 @@ function createHTMLforTodo() {
         if (myTodos[i].isDone) {
             finishedUL.appendChild(li); // Append LI to UL
             li.classList.add("finished"); // Add a class
-            let span = document.createElement("span"); // Create a span for add back button
-            span.classList.add("add-back-icon"); // Add a class to add back button
-            li.appendChild(span) // Append the button the the LI
+            let addBackIcon = document.createElement("span"); // Create a span for add back button
+            addBackIcon.classList.add("add-back-icon"); // Add a class to add back button
+            addBackIcon.setAttribute("title", "Add back"); // Add title attribute to checkboxes that shows a text on hover
+            li.appendChild(addBackIcon) // Append the button the the LI
             checkbox.remove(); // Remove the checkbox
-            span.addEventListener("click", () => {  // Add event listeners to  add back button
+            let removeIcon = document.createElement("span");
+            removeIcon.classList.add("remove-icon")
+            li.appendChild(removeIcon);
+            addBackIcon.addEventListener("click", () => {  // Add event listeners to  add back button
                 markTodoAsNotDone(myTodos[i]);  // If add back button is clicked, call this function
             })
         } else {
@@ -75,8 +85,9 @@ function markTodoAsDone(objectToMarkAsDone) {
 }
 
 // Function to add new todos
-function addNewTodo() {
-    let input = document.getElementById("input").value; // Grab the text from the input element
+function addNewTodo(e) {
+    e.preventDefault();
+    let input = document.getElementById("input").value.trim(); // Grab the text from the input element
     if (input === "") { // If the input field is empty....
         alert("You forgot to write something!") // Create an alert
     } else { // If not,
@@ -91,7 +102,6 @@ function markTodoAsNotDone(objectToMarkAsDone) {
     objectToMarkAsDone.isDone = false; // Mark todo as done
     createHTMLforTodo(); // Create new HTML
 }
-
 
 
 
